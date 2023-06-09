095b15ed275340cb2ba6f115d74f18a2bc0655c1
Ярослав Буторин <yaroslav1234321@gmail.com>
2023-06-09 14:39:13 +0300

    updated

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,6 @@
-from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from api.views import IngredientsViewSet, RecipeViewSet, TagsViewSet
@@ -12,4 +14,4 @@ router.register('recipes', RecipeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
