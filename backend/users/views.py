7081b868af441f78305c090023973cf3d1f5c24d
yaroslavbutorin <yaroslav1234321@gmail.com>
2023-06-12 12:47:48 +0300

    update

--- backend/users/views.py
+++ backend/users/views.py
@@ -16,7 +16,7 @@ User = get_user_model()
 class CustomUserViewSet(UserViewSet):
     pagination_class = LimitPageNumberPagination
 
-    @action(detail=True, permission_classes=[IsAuthenticated])
+    @action(detail=True,methods=['post'], permission_classes=[IsAuthenticated])
     def subscribe(self, request, id=None):
         user = request.user
         author = get_object_or_404(User, id=id)
