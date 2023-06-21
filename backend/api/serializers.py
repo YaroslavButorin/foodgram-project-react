348992fa4889a294e2d6de8ee98f14ca09deacc1
Ярослав Буторин <yaroslav1234321@gmail.com>
2023-06-21 11:52:50 +0300

    update

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -13,6 +13,14 @@ class TagSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class IngredientSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
+
+
 class IngredientAmountSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField(source='ingredient.id')
     name = serializers.ReadOnlyField(source='ingredient.name')
@@ -29,12 +37,6 @@ class IngredientAmountSerializer(serializers.ModelSerializer):
                 fields=['ingredient', 'recipe']
             )
         ]
-
-class IngredientInRecipeWriteSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(write_only=True)
-    class Meta:
-        model = IngredientAmount
-        fields = ('id', 'amount')
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -71,9 +73,9 @@ class RecipeSerializer(serializers.ModelSerializer):
 class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
     author = CustomUserSerializer(read_only=True)
     tags = TagSerializer(read_only=True, many=True)
-    ingredients = IngredientInRecipeWriteSerializer(
+    ingredients = IngredientAmountSerializer(
+        source='ingredientamount_set',
         many=True,
-        read_only=False,
     )
 
     image = Base64ImageField()
